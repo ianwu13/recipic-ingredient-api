d911aa9096ea3a2f21d585a21d8a0851bce1e364
ianwu13 <ianwu13@me.com>
2022-07-15 21:58:58 -0500

    quick fix

--- app/main.py
+++ app/main.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
  
 @app.route("/", methods=['GET', 'POST'])
 def home_view():
-        return "<h1>Welcome to Geeks for Geeks</h1>"
+    return "<h1>Welcome to Geeks for Geeks</h1>"
